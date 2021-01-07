95207966c4ff392cf88d685dccbe6943e711114b
Maciej Rucinski <rucinski.maciej@gmail.com>
2021-01-07 03:22:56 +0100

    ADD: searchBarInputTest()

--- src/test/java/online/rumac/tests/tab/web/WebViewTest.java
+++ src/test/java/online/rumac/tests/tab/web/WebViewTest.java
@@ -8,9 +8,9 @@ import org.testng.annotations.Test;
 
 public class WebViewTest extends BaseSetup {
 
-    @Test(groups = {"inProgress"})
+    @Test(groups = "broken")
     void websiteInputTest() throws InterruptedException {
-        Log.onTerminal("Initiate websiteInputTest");
+        Log.onTerminal("Attempting websiteInputTest");
         MobileElement element = driver.finder().findElement(By.ID("com.amazonaws.devicefarm.android.referenceapp:id/website_input"));
         element.click();
         String sendKeys = "http://www.viacom.tech";
@@ -20,4 +20,16 @@ public class WebViewTest extends BaseSetup {
         Thread.sleep(3000); // to see the output
     }
 
+
+    @Test(groups = "broken")
+    void searchBarInputTest() throws InterruptedException {
+        Log.onTerminal("Attempting searchBarInputTest");
+        MobileElement element = driver.finder().findElement(By.XPath("//android.webkit.WebView[@content-desc=\"Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more\"]/android.view.View[1]/android.view.View/android.view.View[4]/android.widget.EditText"));
+        element.click();
+        String keys = "Clean code";
+        element.sendKeys(keys);
+        driver.keyboard().sendKeyCode(66);
+        Log.onTerminal("Send keys: " + keys);
+        Thread.sleep(3000);
+    }
 }
