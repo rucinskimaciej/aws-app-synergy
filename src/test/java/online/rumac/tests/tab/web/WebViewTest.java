26242cb43a20ee50acb0670f6e7d20d77536e160
Maciej Rucinski <rucinski.maciej@gmail.com>
2021-01-08 00:09:59 +0100

    FIX: add missing line showing WEB view

--- src/test/java/online/rumac/tests/tab/web/WebViewTest.java
+++ src/test/java/online/rumac/tests/tab/web/WebViewTest.java
@@ -1,16 +1,23 @@
 package online.rumac.tests.tab.web;
 
 import com.synergy.core.driver.By;
+import com.synergy.core.driver.elements.Element;
 import com.synergy.core.driver.elements.MobileElement;
+import com.synergy.core.driver.elements.NativeElement;
 import online.rumac.common.logger.Log;
+import online.rumac.common.views.Menu;
+import online.rumac.common.views.MenuElementsView;
 import online.rumac.tests.common.BaseSetup;
 import org.testng.annotations.Test;
 
 public class WebViewTest extends BaseSetup {
 
+
+
     @Test(groups = "broken")
     void websiteInputTest() throws InterruptedException {
         Log.onTerminal("Attempting websiteInputTest");
+        MenuElementsView.show(Menu.WEB, driver);
         MobileElement element = driver.finder().findElement(By.ID("com.amazonaws.devicefarm.android.referenceapp:id/website_input"));
         element.click();
         String sendKeys = "http://www.viacom.tech";
@@ -24,6 +31,8 @@ public class WebViewTest extends BaseSetup {
     @Test(groups = "broken")
     void searchBarInputTest() throws InterruptedException {
         Log.onTerminal("Attempting searchBarInputTest");
+        MenuElementsView.show(Menu.WEB, driver);
+        driver.options().setElementTimeout(500);
         MobileElement element = driver.finder().findElement(By.XPath("//android.webkit.WebView[@content-desc=\"Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more\"]/android.view.View[1]/android.view.View/android.view.View[4]/android.widget.EditText"));
         element.click();
         String keys = "Clean code";
