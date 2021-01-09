d213a024c5d66f5cdd66da1fb27ac24358a13a9b
Maciej Rucinski <rucinski.maciej@gmail.com>
2021-01-09 01:23:16 +0100

    IMPLEMENT: NativeCOmponentsViewTest with ImageCollectionTest subclass

---
+++ src/test/java/online/rumac/tests/tab/nativeComponents/ImageCollectionTest.java
@@ -0,0 +1,36 @@
+package online.rumac.tests.tab.nativeComponents;
+
+import com.synergy.core.driver.By;
+import online.rumac.common.views.tabs.nativeComponents.ImageCollection;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
+import static org.testng.Assert.*;
+
+public class ImageCollectionTest extends NativeComponentsViewTest {
+
+    @Override
+    @BeforeMethod
+    protected void setViewBeforeMethod() {
+        view = new ImageCollection(driver);
+    }
+
+    @Override
+    protected int getTitleXpathIndex() {
+        return 1;
+    }
+
+    @Override
+    protected String getTitle() {
+        return "Image Collection";
+    }
+
+    @Test(groups = "done")
+    void findImagesOnScreen() {
+        assertTrue(((ImageCollection) view).imageIsOnScreen(1 + new Random().nextInt(6)));
+    }
+
+
+}
---
+++ src/test/java/online/rumac/tests/tab/nativeComponents/NativeComponentsViewTest.java
@@ -0,0 +1,37 @@
+package online.rumac.tests.tab.nativeComponents;
+
+import com.synergy.core.driver.By;
+import online.rumac.common.views.tabs.nativeComponents.NativeComponentsView;
+import online.rumac.tests.common.BaseSetup;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public abstract class NativeComponentsViewTest extends BaseSetup {
+
+    protected NativeComponentsView view;
+    protected final String title;
+
+    public NativeComponentsViewTest() {
+        this.title = getTitle();
+    }
+
+    @BeforeMethod
+    protected abstract void setViewBeforeMethod();
+
+    protected abstract int getTitleXpathIndex();
+
+    protected abstract String getTitle();
+
+    protected String getTitleXPath() {
+        return String.format("%s[%d]", titlePath, getTitleXpathIndex());
+    }
+
+    @Test(groups = "done")
+    void findViewTitle() {
+        assertTrue(view.containsTitle(By.XPath(getTitleXPath())));
+    }
+
+    private final String titlePath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.view.ViewPager/android.view.ViewGroup/android.widget.TextView";
+}
