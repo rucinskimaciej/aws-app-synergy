a0d5976af8fcbe7981386f1867b2f1a264bc60b1
Maciej Rucinski <rucinski.maciej@gmail.com>
2021-01-10 17:36:28 +0100

    ADD: scroll to bottom

--- src/test/java/online/rumac/common/interactions/Interactions.java
+++ src/test/java/online/rumac/common/interactions/Interactions.java
@@ -3,17 +3,34 @@ package online.rumac.common.interactions;
 import com.synergy.core.driver.By;
 import com.synergy.core.driver.elements.MobileElement;
 import com.synergy.core.driver.mobile.MobileDriver;
+import com.synergy.core.driver.mobile.MobileScreen;
+import online.rumac.common.logger.Log;
 
 public interface Interactions {
 
     MobileDriver getDriver();
+
+    default Swipe scroll() {
+        return new Swipe(getDriver());
+    }
 
     default boolean contains(String s) {
         MobileElement textElement = getDriver().finder().findElement(By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.view.ViewPager/android.widget.LinearLayout/android.widget.ScrollView/android.widget.TextView"));
         return textElement.getText().contains(s);
     }
 
-    default Swipe scroll() {
-        return new Swipe(getDriver());
+    default void scrollToBottom() {
+        Log.onTerminal("SCROLLING TO BOTTOM");
+        MobileScreen previousScreen;
+        do {
+            previousScreen = getDriver().screen();
+            scroll().down(1);
+        } while (canScrollFurther(previousScreen));
+        Log.onTerminal("SUCCESSFULLY SCROLLED TO BOTTOM");
+    }
+
+    default boolean canScrollFurther(MobileScreen previousScreen) {
+        if (previousScreen == null) return false;
+        return previousScreen.equals(getDriver().screen());
     }
 }
