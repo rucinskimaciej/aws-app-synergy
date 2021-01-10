09c6cef1a23a81f5c7c972de8bb64c3d07ecb1a5
Maciej Rucinski <rucinski.maciej@gmail.com>
2021-01-10 17:58:27 +0100

    MODIFY: generify scrolling in all directions

--- src/test/java/online/rumac/common/interactions/Swipe.java
+++ src/test/java/online/rumac/common/interactions/Swipe.java
@@ -1,6 +1,7 @@
 package online.rumac.common.interactions;
 
 import com.synergy.core.driver.mobile.MobileDriver;
+import com.synergy.core.driver.mobile.MobileScreen;
 
 public class Swipe {
 
@@ -51,6 +52,50 @@ public class Swipe {
                 (int) screen.center.getX(), (int) screen.center.getY(),
                 (int) screen.center.getX(), (int) screen.upEdgeOffset.getY());
     }
+
+    private void scrollToMost(String direction) {
+        MobileScreen previousScreen;
+        do {
+            previousScreen = driver.screen();
+            switchScroll(direction);
+        } while (canScrollFurther(previousScreen));
+    }
+
+    private void switchScroll(String direction) {
+        switch (direction) {
+            case "up" : up(1);
+                return;
+            case "down" : down(1);
+                return;
+            case "left" : left(1);
+                return;
+            case "right" : right(1);
+                return;
+            default: throw new IllegalArgumentException("Unknown direction [ known : up down left right ]");
+        }
+    }
+
+    public void toTop() {
+        scrollToMost("up");
+    }
+
+    public void toBottom() {
+        scrollToMost("down");
+    }
+
+    public void toMostLeft() {
+        scrollToMost("left");
+    }
+
+    public void toMostRight() {
+        scrollToMost("right");
+    }
+
+    private boolean canScrollFurther(MobileScreen previousScreen) {
+        if (previousScreen == null) return false;
+        return previousScreen.equals(driver.screen());
+    }
+
 
     public void setScrollDurationMS(int scrollDurationMS) {
         this.scrollDurationMS = scrollDurationMS;
