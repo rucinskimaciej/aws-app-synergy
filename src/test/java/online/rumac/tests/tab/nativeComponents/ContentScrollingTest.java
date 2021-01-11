908c424d0455b2766cdee30b3e6fb8418caf35da
Maciej Rucinski <rucinski.maciej@gmail.com>
2021-01-11 20:08:05 +0100

    REFACTOR: Cleanup

--- src/test/java/online/rumac/tests/tab/nativeComponents/ContentScrollingTest.java
+++ src/test/java/online/rumac/tests/tab/nativeComponents/ContentScrollingTest.java
@@ -5,17 +5,10 @@ import online.rumac.common.views.tabs.nativeComponents.ContentScrolling;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
 import static org.testng.Assert.*;
 
 
 public class ContentScrollingTest extends NativeComponentsViewTest {
-
-    private static int screenshotCounter = 0;
 
     @BeforeMethod(alwaysRun = true)
     @Override
@@ -28,30 +21,11 @@ public class ContentScrollingTest extends NativeComponentsViewTest {
         return 2;
     }
 
-    @Test(groups = "undone", enabled = true)
+    @Test(groups = "done")
     void findTextAfterScrolling() {
-        view.scroll().down(10);
+        view.scroll().down(5);
         By locator = By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.view.ViewPager/android.widget.LinearLayout/android.widget.ScrollView/android.widget.TextView");
         assertTrue(view.contains("Ut accumsan commodo ex, nec ornare mi malesuada maximus.",locator));
-        saveImage(driver.screen().getImage()); // todo SAVES IMAGE TO DIRECTORY
-    }
-
-    void saveImage(File file) {
-        try {
-            BufferedImage bfrdImg = ImageIO.read(file);
-            File outputFIle = new File(String.format("%s%d%s", "C:\\Users\\rucinskm\\Documents\\ViacomCBS\\aws-app-synergy\\src\\test\\resources\\screenshots\\screenshot", ++screenshotCounter,".png"));
-            ImageIO.write(bfrdImg, "PNG", outputFIle);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    @Test(groups = "undone", enabled = false)
-    void shouldFail() {
-        view.scroll().down(10);
-        By locator = By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.view.ViewPager/android.widget.LinearLayout/android.widget.ScrollView/android.widget.TextView");
-        assertFalse(view.contains("Lorem ipsum dolor sit amet, consectetur adipiscing elit.",locator));
     }
 
     @Override
