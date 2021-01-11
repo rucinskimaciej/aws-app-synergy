eaeca3fc91d61bf6237c718919fe28dd5c57e411
Maciej Rucinski <rucinski.maciej@gmail.com>
2021-01-11 17:35:03 +0100

    MODIFY: make contains() reliant on locator

--- src/test/java/online/rumac/common/interactions/Interactions.java
+++ src/test/java/online/rumac/common/interactions/Interactions.java
@@ -3,7 +3,6 @@ package online.rumac.common.interactions;
 import com.synergy.core.driver.By;
 import com.synergy.core.driver.elements.MobileElement;
 import com.synergy.core.driver.mobile.MobileDriver;
-import com.synergy.core.driver.mobile.MobileScreen;
 
 public interface Interactions {
 
@@ -13,8 +12,8 @@ public interface Interactions {
         return new Swipe(getDriver());
     }
 
-    default boolean contains(String s) {
-        MobileElement textElement = getDriver().finder().findElement(By.XPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.view.ViewPager/android.widget.LinearLayout/android.widget.ScrollView/android.widget.TextView"));
+    default boolean contains(String s, By locator) {
+        MobileElement textElement = getDriver().finder().findElement(locator);
         return textElement.getText().contains(s);
     }
 }
