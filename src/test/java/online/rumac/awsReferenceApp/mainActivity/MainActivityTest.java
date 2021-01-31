1412072a480bc59b55c61c9a1aaff0e0336de525
Maciej Rucinski <rucinski.maciej@gmail.com>
2021-02-01 00:14:21 +0100

    REFACTOR: provide new BaseSetup implementation

--- src/test/java/online/rumac/awsReferenceApp/mainActivity/MainActivityTest.java
+++ src/test/java/online/rumac/awsReferenceApp/mainActivity/MainActivityTest.java
@@ -1,12 +1,17 @@
 package online.rumac.awsReferenceApp.mainActivity;
 
+import com.synergy.core.driver.mobile.MobileDriver;
 import com.synergy.core.driver.mobile.android.AndroidDriver;
 import com.synergy.core.driver.mobile.ios.IOSDriver;
+import online.rumac.common.driver.DriverGenerator;
 import online.rumac.common.logger.Log;
 import online.rumac.common.BaseSetup;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class MainActivityTest extends BaseSetup {
+
+    private MobileDriver driver;
 
     @Test(groups = {"done"})
     void connectionTest() {
@@ -18,5 +23,11 @@ public class MainActivityTest extends BaseSetup {
         if (info != null && !info.isEmpty()) log = "App opened successfully";
         else log = "Could not open app";
         Log.onTerminal(log);
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    @Override
+    public void testSetup() {
+        driver = DriverGenerator.generate();
     }
 }
