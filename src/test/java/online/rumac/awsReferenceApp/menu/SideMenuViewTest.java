4c602470839a3405dcab79d3109bb862ae04e143
Maciej Rucinski <rucinski.maciej@gmail.com>
2021-02-01 01:22:12 +0100

    FIX: remaining tests

--- src/test/java/online/rumac/awsReferenceApp/menu/SideMenuViewTest.java
+++ src/test/java/online/rumac/awsReferenceApp/menu/SideMenuViewTest.java
@@ -1,5 +1,8 @@
 package online.rumac.awsReferenceApp.menu;
 
+import com.synergy.core.driver.mobile.MobileDriver;
+import online.rumac.common.driver.DriverGenerator;
+import online.rumac.common.driver.DriverManager;
 import online.rumac.common.logger.Log;
 import online.rumac.awsReferenceApp.menu.view.SideMenuView;
 import online.rumac.common.BaseSetup;
@@ -9,8 +12,17 @@ public class SideMenuViewTest extends BaseSetup {
 
     @Test(groups = "done")
     void openSideMenuBar() {
-        SideMenuView.getInstance().getViewFor(driver);
+        SideMenuView.getInstance().getViewFor(viewHolder.getDriver());
         Log.onTerminal("Side menu opened successfully");
     }
 
+    @Override
+    protected DriverManager getViewHolder() {
+        return new DriverManager() {
+            @Override
+            public MobileDriver getDriver() {
+                return DriverGenerator.generate();
+            }
+        };
+    }
 }
